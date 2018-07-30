88ae4bd45d20e5adb9a9818cfc667929f0f6b17c
YanaVG <yanavg18@gmail.com>
2018-07-30 22:49:26 +0300

    added gitignore

--- UBS/js/main.js
+++ UBS/js/main.js
@@ -35,15 +35,13 @@ let posts = [
 
 const grid = document.querySelector('.grid');
 const list = document.querySelector('#list').innerHTML.trim();
-
 const template = Handlebars.compile(list);
 
 Handlebars.registerHelper("formatValue", function(property) {
-    let value = property.toString();
-    
+    let value = property.toString(); 
     return new Handlebars.SafeString(
-        "<p class='value_text'>" + value.substring(0, 4) + "</p>" + 
-        "<p class='value_text-bold'>" + value.substring(4, 6) + '</p>' + 
+        "<p class='value_begin'>" + value.substring(0, 4) + "</p>" + 
+        "<p class='value_middle'>" + value.substring(4, 6) + '</p>' + 
         "<h4>" + value.substring(6, 7) + "<h4>"  
     );
 });
@@ -52,15 +50,8 @@ const markup = posts.reduce((acc, post) => acc + template(post), '');
 grid.insertAdjacentHTML('afterbegin', markup);
 
 const wrapUsd = document.querySelectorAll(".wrap_usd");
-// const wrapSellValue = Array.from(document.querySelectorAll(".wrap_sell-value"));
-// const wrapBuyValue = Array.from(document.querySelectorAll(".wrap_buy-value"));
-const wrapSellValue = document.querySelector(".wrap_sell-value");
-const wrapBuyValue = document.querySelectorAll(".wrap_buy-value");
-const valueText = document.querySelector(".value_text");
-const valueTextBold = document.querySelector(".value_text-bold");
-const valueTextUpper = document.querySelector(".wrap_sell-value h4");
 
-const arrow = document.querySelector(".arrow"); 
+// const arrow = document.querySelector(".arrow"); 
 
 function getRandom10pc(num){
     return Math.random() * num / 10;
@@ -79,47 +70,27 @@ function randomData (arr) {
 };
 
 window.setInterval(function() {
-    posts = randomData(posts)
+    randomData(posts)
 }, 1000);
 
-function updateDate() {
-    posts.forEach(post => {
-        let postKey = post.pair;
-        wrapUsd.forEach(item => {
-            let dataSetItem = item.dataset.sell;
-            if(dataSetItem === postKey) { 
-                // console.log(dataSetItem, postKey);
-                let postSellString = post.sell.toString();
-                valueText.textContent = postSellString.substring(0, 4);
-                valueTextBold.textContent = postSellString.substring(4, 6);
-                valueTextBold.textContent = postSellString.substring(6, 7);
-                //  wrapSellValue.textContent = (post.sell).toFixed(5);
-                //  wrapBuyValue.textContent = (post.buy).toFixed(5);
-            }   
-        })
-});
-if(getRandomSign() > 0) {
-    arrow.classList.remove("red_arrow"); 
-    arrow.classList.add("green_arrow");
-} else {
-    arrow.classList.remove("green_arrow");
-    arrow.classList.add("red_arrow");  
-}
-}
+function update() {
+    const arr = randomData(posts);
+    Array.from(wrapUsd, el => {
+        const key = el.dataset.sell;
+        let obj = arr.filter(el => el.pair === key);
+        let buy = obj[0].buy;
+        let sell = obj[0].sell;
+        let buyToString = buy.toString();
+        let sellToString = sell.toString();
+        el.querySelector('.wrap_sell .value_begin').textContent = sellToString.substring(0, 4);
+        el.querySelector('.wrap_sell .value_middle').textContent = sellToString.substring(4, 6);
+        el.querySelector('.wrap_sell h4').textContent = sellToString.substring(6, 7);
+        el.querySelector('.wrap_buy .value_begin').textContent = buyToString.substring(0, 4);
+        el.querySelector('.wrap_buy .value_middle').textContent = buyToString.substring(4, 6);
+        el.querySelector('.wrap_buy h4').textContent = buyToString.substring(6, 7);
+    });
+};
+
 window.setInterval(function() {
-    updateDate();
+    update();
 }, 1000);
-
-
-
-
-
-        // dataSetArr.find(key => {
-        //     if(key === postKey && wrapUsd.dataSet === postKey) {
-        //         console.log(key, postKey); 
-        //         // wrapSellValue.textContent = (post.sell).toFixed(5);
-        //         // wrapBuyValue.textContent = (post.buy).toFixed(5);
-        //     } else {
-        //         console.log('mistatake');
-        //     }
-        // }) 
