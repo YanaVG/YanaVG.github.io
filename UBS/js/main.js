1a0922cd3818cac5f6dbed0154a391affb6ea1c2
YanaVG <yanavg18@gmail.com>
2018-07-31 18:10:34 +0300

    ready task

--- UBS/js/main.js
+++ UBS/js/main.js
@@ -2,47 +2,47 @@
 
 let posts = [
     {
-        "pair": "USD CHF", 
-        "buy":  0.99143, 
+        "pair": "USD CHF",
+        "buy": 0.99143,
         "sell": 0.99043
     },
     {
-        "pair": "GBP USD", 
-        "buy":  1.28495, 
+        "pair": "GBP USD",
+        "buy": 1.28495,
         "sell": 1.2836
     },
     {
-        "pair": "GBP CHF", 
-        "buy":  1.27378, 
+        "pair": "GBP CHF",
+        "buy": 1.27378,
         "sell": 1.27147
     },
     {
-        "pair": "EUR SEK", 
-        "buy":  9.632, 
+        "pair": "EUR SEK",
+        "buy": 9.632,
         "sell": 9.6055
     },
     {
-        "pair": "USD JPY", 
-        "buy":  110.467, 
+        "pair": "USD JPY",
+        "buy": 110.467,
         "sell": 110.417
     },
     {
-        "pair": "EUR JPY", 
-        "buy":  120.589, 
+        "pair": "EUR JPY",
+        "buy": 120.589,
         "sell": 120.491
     }
-  ];
+];
 
 const grid = document.querySelector('.grid');
 const list = document.querySelector('#list').innerHTML.trim();
 const template = Handlebars.compile(list);
 
-Handlebars.registerHelper("formatValue", function(property) {
-    let value = property.toString(); 
+Handlebars.registerHelper("formatValue", function (property) {
+    let value = property.toString();
     return new Handlebars.SafeString(
-        "<p class='value_begin'>" + value.substring(0, 4) + "</p>" + 
-        "<p class='value_middle'>" + value.substring(4, 6) + '</p>' + 
-        "<h4>" + value.substring(6, 7) + "<h4>"  
+        "<p class='value_begin'>" + value.substring(0, 4) + "</p>" +
+        "<p class='value_middle'>" + value.substring(4, 6) + '</p>' +
+        "<h4>" + value.substring(6, 7) + "<h4>"
     );
 });
 
@@ -51,22 +51,45 @@ grid.insertAdjacentHTML('afterbegin', markup);
 
 const wrapUsd = document.querySelectorAll(".wrap_usd");
 
-function getRandom10pc(num){
+
+
+/**
+ *
+ *
+ * @param {number} num
+ * @returns tenth part of num
+ */
+function getRandom10pc(num) {
     return Math.random() * num / 10;
 };
-  
-function getRandomSign() { 
+
+/**
+ *
+ *
+ * @returns value +/-1
+ */
+function getRandomSign() {
     return Math.random() > 0.5 ? -1 : 1;
 };
-  
-function randomData (arr) {
+
+/**
+ *
+ *
+ * @param {obj} arr
+ * @returns array with new values "buy" & "sell"
+ */
+function randomData(arr) {
     return arr.map(el => ({
-    "pair": el["pair"],
-    "buy": el["buy"] + getRandomSign() * getRandom10pc(el["buy"]),
-    "sell": el["sell"] + getRandomSign() * getRandom10pc(el["sell"])
-  }));
+        "pair": el["pair"],
+        "buy": el["buy"] + getRandomSign() * getRandom10pc(el["buy"]),
+        "sell": el["sell"] + getRandomSign() * getRandom10pc(el["sell"])
+    }));
 };
 
+/**
+ * @description Function update value of buy and sell in every grid_item
+ * and show red/green arrow depends on difference between buy and sell values every 1000ms
+ */
 function update() {
     const arr = randomData(posts);
 
@@ -83,19 +106,19 @@ function update() {
         el.querySelector('.wrap_buy .value_begin').textContent = buyToString.substring(0, 4);
         el.querySelector('.wrap_buy .value_middle').textContent = buyToString.substring(4, 6);
         el.querySelector('.wrap_buy h4').textContent = buyToString.substring(6, 7);
-        
+
         let arrow = el.querySelector(".arrow");
-      
-        if(buyToString > sellToString) {
-            arrow.classList.remove("red_arrow"); 
+
+        if (buyToString > sellToString) {
+            arrow.classList.remove("red_arrow");
             arrow.classList.add("green_arrow");
         } else {
             arrow.classList.remove("green_arrow");
-            arrow.classList.add("red_arrow"); 
+            arrow.classList.add("red_arrow");
         }
     });
 };
 
-window.setInterval(function() {
+window.setInterval(function () {
     update();
 }, 1000);
